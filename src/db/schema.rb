6ec95dad33891012f81bd36bbd6a51945c059bca
Student 1 Account <student1@hpc257.tcnj.edu>
2022-05-11 20:33:54 -0400

    migration for type of location

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_19_212455) do
+ActiveRecord::Schema.define(version: 2022_05_12_002912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2022_04_19_212455) do
     t.string "city"
     t.string "state"
     t.string "zipcode"
+    t.boolean "dropoff"
+    t.boolean "aid"
     t.index ["directory_id"], name: "index_entries_on_directory_id"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
